57ceb0c3c9136e80da2d0f6c4ff3b9bc648f9bef
mugeshi <nicolemugeshi@gmail.com>
2024-01-04 12:27:20 +0300

    update baricons

--- app/src/components/Navbar.js
+++ app/src/components/Navbar.js
@@ -4,10 +4,17 @@ import { MenuItems } from "./MenuItems";
 import { Link } from "react-router-dom";
 
 class Navbar extends Component {
+  state={clicked: false};
+  handleclick = () => {
+    this.setState({ clicked: !this.state.clicked})
+  }
   render() {
     return (
       <nav className="NavbarItems">
         <h1 className="navbar-logo">Nyika</h1>
+         <div className="menu-icons" onClick={this.handleclick}>
+           <i className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
+         </div>
         <ul className="nav-menu">
           {MenuItems.map((item, index) => {
             return (
