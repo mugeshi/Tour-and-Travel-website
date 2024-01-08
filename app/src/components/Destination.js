261988c50a86066bcc734a18d3f2ae505170118c
mugeshi <nicolemugeshi@gmail.com>
2024-01-08 10:31:22 +0300

    updating Destination

---
+++ app/src/components/Destination.js
@@ -0,0 +1,12 @@
+const Destination = () => {
+    return (
+        <div className="destination">
+            <h1>Popular Destination</h1>
+            <p>Tours give you the opportunity have unforgettable experience. </p>
+
+        </div>
+    );
+
+};
+
+export default Destination;
