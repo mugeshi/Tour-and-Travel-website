a1e261b8d91bd5719cc6a3550b314a79fa8e5a75
mugeshi <nicolemugeshi@gmail.com>
2024-01-13 20:04:43 +0300

    updated Nyika

---
+++ app/src/components/Nyika.js
@@ -0,0 +1,13 @@
+import "./NyikaStyles.css"
+
+function Nyika(){
+    return(
+       <div className="Nyika">
+        <h1>Recent Trips</h1>
+        <p>You can discover unique destinations using google maps</p>
+       </div> 
+       
+    )
+}
+
+export  default Nyika
