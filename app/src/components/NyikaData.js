30e7f4f78aa3fbdea50bf014caf3cca26e543761
mugeshi <nicolemugeshi@gmail.com>
2024-01-13 20:40:30 +0300

    updated Nyika

---
+++ app/src/components/NyikaData.js
@@ -0,0 +1,14 @@
+import "./NyikaStyles.css";
+
+
+function NyikaData(){
+    return(
+    < div className="N-card">
+        <div className="N-image">
+       <img src={Props.image} alt="image/" /> 
+    </div>
+    <h4>{Props.heading} </h4>
+    <p>{props.text}</p>
+    </div>
+    )
+}
