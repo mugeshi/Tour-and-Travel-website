4adfa9d6f457cc8babf92c05da1d5a43ec1ab94e
mugeshi <nicolemugeshi@gmail.com>
2024-01-20 18:32:28 +0300

    updtated form

--- app/src/components/ContactForm.js
+++ app/src/components/ContactForm.js
@@ -6,6 +6,10 @@ function ContactForm (){
             <h1>Send a message to Us!</h1>
             <form>
                <input placeholder="Name"/>
+               <input placeholder="Email"/>
+               <input placeholder="Subject"/>
+               <textarea placeholder="Message" rows="4"></textarea>
+               <button>Send Message</button>
             </form>
         </div>
         
--- app/src/routes/Contact.js
+++ app/src/routes/Contact.js
@@ -2,6 +2,7 @@ import Hero from "../components/Hero";
 import Navbar from "../components/Navbar"
 import AboutImg from "../assets/Giraffes.jpg"
 import Footer from "../components/Footer";
+import ContactForm from "../components/ContactForm";
 
 function Contact (){
    return(
@@ -13,6 +14,7 @@ function Contact (){
     tittle="Contact"
     
     />
+     <ContactForm />
      <Footer />
     </>
    )
