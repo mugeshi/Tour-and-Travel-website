70da7f59922a5e2002c66cb83c4ec1e1c3ce0b7a
mugeshi <nicolemugeshi@gmail.com>
2024-01-20 17:13:23 +0300

    updated AboutUs page

---
+++ app/src/components/AboutUs.js
@@ -0,0 +1,30 @@
+import "./AboutUsStyles.css"
+
+function AboutUs () {
+    return(
+       <div className="about-container">
+        <h1>Our History</h1>
+        <p>Founded with a love for exploration and a commitment to celebrating 
+            the richness of diverse cultures, Nyika began its journey in year 
+            of establishment. Since then, we have grown into a trusted name 
+            in the travel industry, curating experiences that blend adventure, 
+            cultural discovery, and environmental responsibility. Our history is 
+            marked by a dedication to creating journeys that leave a positive impact, 
+            ensuring every traveler's story with Nyika is one of joy, learning, and 
+            meaningful connections.</p>
+             <h1>Our Mission</h1>
+        <p>At Nyika, our mission is to inspire and facilitate unforgettable journeys, fostering a 
+            deep connection between individuals and the diverse beauty of the world. 
+            We aim to provide exceptional travel experiences that go beyond exploration, 
+            creating lasting memories and a sense of wonder.</p>
+             <h1>Our Vision</h1>
+        <p>Our vision at Nyika is to be a leading global platform for immersive travel, 
+            recognized for our commitment to authenticity, sustainability, and personalized 
+            service. We aspire to be the go-to choice for individuals seeking unique and enriching 
+            adventures, driven by a passion for discovery and cultural exchange.</p>
+       </div>
+       
+    )
+}
+
+export default AboutUs
--- app/src/routes/About.js
+++ app/src/routes/About.js
@@ -2,17 +2,19 @@ import Hero from "../components/Hero";
 import Navbar from "../components/Navbar"
 import AboutImg from "../assets/Africa2.png"
 import Footer from "../components/Footer";
+import AboutUs from "../components/AboutUs";
 
 function About (){
    return(
     <>
    <Navbar />  
     <Hero 
-    cName="hero-mid "
+     cName="hero-mid "
     heroimg={AboutImg}
     tittle="About"
     
     />
+    <AboutUs />
     <Footer />
     </>
    )
